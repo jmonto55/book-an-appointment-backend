11e9dadab76f9222374c3dda73c283af083bde4c
SimonGideon <simongideon918@gmail.com>
2023-06-08 19:41:23 +0300

    Date validation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,6 +2,6 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :house
 
-  validates :check_in, presence: true, timeliness: { type: :date }
-  validates :check_out, presence: true, timeliness: { type: :date }
+  validates :check_in, presence: true
+  validates :check_out, presence: true
 end
