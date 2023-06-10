ddfe82f5b4d66dd8ff897ea5617e1f9aee6669d0
SimonGideon <simongideon918@gmail.com>
2023-06-10 15:22:38 +0300

    house controller end-point testing

--- spec/integration/houses_spec.rb
+++ spec/integration/houses_spec.rb
@@ -27,5 +27,48 @@ RSpec.
         run_test!
       end
     end
+
+    get "Retrieves all houses" do
+      tags "Houses"
+      produces "application/json", "application/xml"
+      response "200", "houses found" do
+        schema type: :array,
+               items: { "$ref" => "#/components/schemas/house" }
+        run_test!
+      end
+    end
+  end
+
+  path "/houses/{id}" do
+    get "Retrieves a house" do
+      tags "Houses"
+      produces "application/json", "application/xml"
+      parameter name: :id, in: :path, type: :string
+      response "200", "house found" do
+        schema type: :object,
+               properties: {
+                 id: { type: :integer },
+                 name: { type: :string },
+                 address: { type: :string },
+                 night_price: { type: :integer },
+                 city: { type: :string },
+                 description: { type: :string },
+                 photo: { type: :string },
+               },
+               required: %w[id name address night_price description photo]
+        let!(:id) { FactoryBot.create(:house).id }
+        run_test!
+      end
+    end
+
+    delete "Delete house" do
+      tags "Houses"
+      consumes "application/json", "application/xml"
+      parameter name: :id, in: :path, type: :string
+      response "204", "house deleted" do
+        let(:id) { FactoryBot.create(:house).id }
+        run_test!
+      end
+    end
   end
 end
