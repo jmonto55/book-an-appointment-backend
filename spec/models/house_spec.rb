0944e3b756e904763d3ba76eafa3518bfffacc15
SimonGideon <simongideon918@gmail.com>
2023-06-21 18:39:40 +0300

    fix tests

--- spec/models/house_spec.rb
+++ spec/models/house_spec.rb
@@ -1,5 +1,11 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe House, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+  end
+
+  describe "associations" do
+    it { should have_many(:students) }
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { should validate_presence_of(:username) }
+  end
+
+  describe "associations" do
+    it { should have_many(:reservations) }
+  end
 end
