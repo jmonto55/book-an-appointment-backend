92f47825980ba3205d71bfbfe3f6b7fcfbc29446
SimonGideon <simongideon918@gmail.com>
2023-06-21 19:30:29 +0300

    Solve linters

--- spec/helpers/current_user_helper_spec.rb
+++ spec/helpers/current_user_helper_spec.rb
@@ -1,10 +1,10 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe CurrentUserHelper, type: :helper do
-  describe "#current_user" do
-    let(:user) { User.create(email: "user@example.com", password: "password123") }
+  describe '#current_user' do
+    let(:user) { User.create(email: 'user@example.com', password: 'password123') }
 
-    it "returns the current user" do
+    it 'returns the current user' do
       allow(helper).to receive(:current_user).and_return(user)
       expect(helper.current_user).to eq(user)
     end
